339f8f41e674545bf3b46db5a15814072f82e2d5
Sabrina Lesser <slesser6@jh.edu>
2024-07-12 21:42:31 -0400

    add code to main

--- app/src/main/java/com/spiderb2/pokemon/Main.java
+++ app/src/main/java/com/spiderb2/pokemon/Main.java
@@ -1,8 +1,18 @@
 package com.spiderb2.pokemon;
 
+import com.spiderb2.pokemon.pokemon.Pikachu;
+import com.spiderb2.pokemon.pokemon.Pokemon;
+import com.spiderb2.pokemon.pokemon.moves.PokemonMove;
 
 public class Main {
     public static void main(String[] args){
-        System.out.println("Hello World");
+        Pokemon pika1 = new Pikachu();
+        Pikachu pika2 = new Pikachu("Sparky");
+
+        System.out.println(pika1.getName()+" has "+pika1.getHP()+" HP.");
+        System.out.println(pika2.getName()+" has "+pika2.getHP()+" HP.");
+        for(PokemonMove move: pika2.getMoves()){
+            move.doMove();
+        }
     }
 }
